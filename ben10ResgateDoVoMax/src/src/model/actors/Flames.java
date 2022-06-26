7ff6f4ed6e064d6c04180c5193e0a6a1d576982c
Tiago Perrupato Antunes <tiagoperrupato@gmail.com>
2022-06-26 20:56:59 -0300

    criando bola de fogo e seu ataque

--- ben10ResgateDoVoMax/src/src/model/actors/Flames.java
+++ ben10ResgateDoVoMax/src/src/model/actors/Flames.java
@@ -8,6 +8,19 @@ public class Flames extends Hero {
 		super(posRow, posColumn, typeActor);
 	}
 	
-	
+	// não implementa nada nessa pois eh imune a lava
 	public void searchLavaPool(String lavaPool, ArrayList<IActor> cellActors) {}
+	
+	
+	public void attack() {
+		
+		int posRow = this.getPosRow(), posColumn = this.getPosColumn();
+		
+		if (posColumn+1 >= 0 && posColumn+1 < this.getRoom().getQtyColumns()) {
+			FireBall fireBall = new FireBall(posRow, posColumn+1, "FB");
+			fireBall.connect(this.getRoom());
+			fireBall.insert();
+			fireBall.attack();
+		}
+	}
 }
