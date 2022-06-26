af3c520533b0c6ce573fcc95d0b773ec5eb88d41
Tiago Perrupato Antunes <tiagoperrupato@gmail.com>
2022-06-26 17:19:32 -0300

    fazendo heroi nao atravessar barreiras

---
+++ ben10ResgateDoVoMax/src/src/model/actors/LavaPool.java
@@ -0,0 +1,9 @@
+package model.actors;
+
+public class LavaPool extends Actor {
+	
+	public LavaPool(int posRow, int posColumn, String typeActor) {
+		
+		super(posRow, posColumn, typeActor);
+	}
+}
---
+++ ben10ResgateDoVoMax/src/src/model/actors/WoodBox.java
@@ -0,0 +1,9 @@
+package model.actors;
+
+public class WoodBox extends Actor {
+	
+	public WoodBox(int posRow, int posColumn, String typeActor) {
+		
+		super(posRow, posColumn, typeActor);
+	}
+}
